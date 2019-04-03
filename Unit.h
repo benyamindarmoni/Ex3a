6bfb5da3e078438debb3c4213edc4815657d9d8e
DZand <48325754+DZand@users.noreply.github.com>
2019-04-03 14:35:30 +0300

    Create Unit.h

---
+++ Unit.h
@@ -0,0 +1,40 @@
+//
+
+// Created by Danielle2 on 02/04/2019.
+
+//
+
+#pragma once
+
+
+
+namespace ariel
+
+{
+
+    enum class Unit
+
+    {
+
+        M,
+
+        KM,
+
+        CM,
+
+        SEC,
+
+        MIN,
+
+        HOUR,
+
+        G,
+
+        KG,
+
+        TON
+
+    };
+
+}
+
