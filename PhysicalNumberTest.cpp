f66e74dd7354a8d25686fb4e5a6b9d991918782b
Benyamin Darmoni <48030406+benyamindarmoni@users.noreply.github.com>
2019-04-07 21:07:24 +0300

    Update PhysicalNumberTest.cpp

--- PhysicalNumberTest.cpp
+++ PhysicalNumberTest.cpp
@@ -1,73 +1,58 @@
-/**
-
- * Examples of automatic tests for the exercise on physical numbers.
-
- *
-
- * @author Erel Segal-Halevi
-
- * @since 2019-02
-
- */
-
-
 
 #include <iostream>
-
-using std::cout, std::endl;
-
+#include <sstream>
+using std::cout, std::endl, std::istringstream;
 #include "PhysicalNumber.h"
-
 using ariel::PhysicalNumber, ariel::Unit;
-
 #include "badkan.hpp"
 
-
-
 int main() {
+  badkan::TestCase testcase;
+  int grade=0;
+  int signal = setjmp(badkan::longjmp_buffer);
+  if (signal == 0) {
 
-  PhysicalNumber a(2, Unit::KM);
+    // BASIC TESTS - DO NOT CHANGE
+    PhysicalNumber a(2, Unit::KM);
+    PhysicalNumber b(300, Unit::M);
+    PhysicalNumber c(2, Unit::HOUR);
+    PhysicalNumber d(30, Unit::MIN);
 
-  PhysicalNumber b(300, Unit::M);
+    testcase
+    .setname("Basic output")
+    .CHECK_OUTPUT(a, "2[km]")
+    .CHECK_OUTPUT(b, "300[m]")
 
-  PhysicalNumber c(2, Unit::HOUR);
+    .setname("Compatible dimensions")
+    .CHECK_OUTPUT(b+a, "2300[m]")
+    .CHECK_OUTPUT((a+=b), "2.3[km]")
+    .CHECK_OUTPUT(a, "2.3[km]")
+    .CHECK_OUTPUT(a+a, "4.6[km]")
+    .CHECK_OUTPUT(b-b, "0[m]")
+    .CHECK_OUTPUT(c, "2[hour]")
+    .CHECK_OUTPUT(d, "30[min]")
+    .CHECK_OUTPUT(d+c, "150[min]")
 
-  PhysicalNumber d(30, Unit::MIN);
+    .setname("Incompatible dimensions")
+    .CHECK_THROWS(a+c)
+    .CHECK_THROWS(a+d)
+    .CHECK_THROWS(b+c)
+    .CHECK_THROWS(b+d)
 
-  badkan::TestCase ("Physical number")
+    .setname("Basic input")
+    .CHECK_OK(istringstream("700[kg]") >> a)
+    .CHECK_OUTPUT((a += PhysicalNumber(1, Unit::TON)), "1700[kg]")
 
-  .CHECK_OUTPUT(a, "2[km]")
+    // YOUR TESTS - INSERT AS MANY AS YOU WANT
 
-  .CHECK_OUTPUT(b, "300[m]")
+      .setname("...")
 
-  .CHECK_OUTPUT(b+a, "2300[m]")
-
-  .CHECK_OUTPUT(a+=b, "2.3[km]")
-
-  .CHECK_OUTPUT(a, "2.3[km]")
-
-  .CHECK_OUTPUT(a+a, "4.6[km]")
-
-  .CHECK_OUTPUT(b-b, "0[m]")
-
-  .CHECK_OUTPUT(c, "2[hour]")
-
-  .CHECK_OUTPUT(d, "30[min]")
-
-  .CHECK_OUTPUT(d+c, "150[min]")
-
-  .CHECK_THROWS(a+c)
-
-  .CHECK_THROWS(a+d)
-
-  .CHECK_THROWS(b+c)
-
-  .CHECK_THROWS(b+d)
-
-  .CHECK_OK("700[kg]" >> a)
-
-  .CHECK_OUTPUT(a += PhysicalNumber(1, Unit::TON), "1700[kg]")
-
-  .print();
-
+      .print(cout, /*show_grade=*/false);
+      grade = testcase.grade();
+    } else {
+      testcase.print_signal(signal);
+      grade = 0;
+    }
+    cout <<  "*** Grade: " << grade << " ***" << endl;
+    return grade;
 }
