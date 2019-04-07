524e6b2dd2d1973d582de08926ae03b9f509fa3a
DZand <48325754+DZand@users.noreply.github.com>
2019-04-07 13:21:49 +0300

    Update PhysicalNumber.cpp

--- PhysicalNumber.cpp
+++ PhysicalNumber.cpp
@@ -1,159 +1,101 @@
 #include <iostream>
+
 #include "PhysicalNumber.h"
+
 using namespace std;
+
 bool ariel::PhysicalNumber::operator==(const PhysicalNumber &rhs)
-
 {
-
     return false;
-
 }
-
-
-
 bool ariel::PhysicalNumber::operator!=(const PhysicalNumber &rhs)
-
 {
-
     return false;
-
 }
-
-
-
 bool ariel::PhysicalNumber::operator<(const PhysicalNumber &rhs)
-
 {
-
     return false;
-
 }
-
-
-
 bool ariel::PhysicalNumber::operator>(const PhysicalNumber &rhs)
-
 {
-
     return false;
-
 }
-
-
-
 bool ariel::PhysicalNumber::operator<=(const PhysicalNumber &rhs)
-
 {
-
     return false;
-
 }
-
-
-
 bool ariel::PhysicalNumber::operator>=(const PhysicalNumber &rhs)
-
 {
-
     return false;
-
 }
-
-
-
 ariel::PhysicalNumber &ariel::operator+=(ariel::PhysicalNumber &rhs,const ariel::PhysicalNumber &lhs)
-
 {
-
     return rhs;
-
 }
-
-
-
 ariel::PhysicalNumber& ariel::PhysicalNumber::operator-=(PhysicalNumber &rhs)
-
 {
-
     return *this;
-
 }
-
-
 
 ariel::PhysicalNumber& ariel::PhysicalNumber::operator+(const PhysicalNumber &rhs)
-
 {
-
     return *this;
-
 }
-
-
-
 ariel::PhysicalNumber& ariel::PhysicalNumber::operator-(const PhysicalNumber &rhs)
-
 {
-
     return *this;
-
 }
-
-
-
 ariel::PhysicalNumber& ariel::PhysicalNumber::operator-()
-
 {
-
     return *this;
-
 }
-
-
-
 ariel::PhysicalNumber& ariel::PhysicalNumber::operator+()
-
 {
-
     return *this;
-
 }
-
-
-
 ariel::PhysicalNumber& ariel::PhysicalNumber::operator++()
-
 {
-
     return *this;
-
 }
-
-
-
 ariel::PhysicalNumber& ariel::PhysicalNumber::operator--()
-
 {
-
     return *this;
-
 }
-
-
 istream& ariel::operator>>(istream &in, ariel::PhysicalNumber &a)
-
 {
-
     return in;
-
 }
 ostream& ariel::operator<<(ostream &out,const ariel::PhysicalNumber &a)
-
 {
-
-    return out;
-
+    return out<<a.value<<"["<<"]"<<endl;
 }
+
+string ariel::PhysicalNumber::getType()
+{
+    int tempUnit=(int)(this->unit);
+    switch (tempUnit)
+    {
+        case 0: return "M";
+        case 1: return "KM";
+        case 2: return "CM";
+        case 3: return "SEC";
+        case 4: return "MIN";
+        case 5: return "HOUR";
+        case 6: return "G";
+        case 7: return "KG";
+        case 8: return "TON";
+    }
+    return "NULL";
+}
+
+double ariel::PhysicalNumber::getData()
+{
+    return this->value;
+}
+
+
+
+
 
 
 
