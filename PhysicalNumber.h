9f5b76527a6c6b81823add322a202ea86e0b21e7
Benyamin Darmoni <48030406+benyamindarmoni@users.noreply.github.com>
2019-04-14 17:03:12 +0300

    Update PhysicalNumber.h

--- PhysicalNumber.h
+++ PhysicalNumber.h
@@ -1,49 +1,93 @@
-//
-
-
-
-// Created by Danielle2 on 02/04/2019.
-
-
-
-//
 #pragma once
+
 #include "Unit.h"
+
 #include <iostream>
+
 using namespace std;
+
 namespace ariel
+
 {
+
     class PhysicalNumber
+
     {
+
     public:
+
         double value;
+
         Unit unit;
-        PhysicalNumber(double num, Unit type):value(num),unit(type)
+        PhysicalNumber(const PhysicalNumber &a)
         {
-           value=num;
-            unit=type;
+            this->value=a.value;
+            this->unit=a.unit;
         }
+
+        PhysicalNumber(double num, Unit type):value(num),unit(type)
+
+        {
+
+            value=num;
+
+            unit=type;
+
+        }
+
         //boolean
+
         bool operator==(const PhysicalNumber& rhs);
+
         bool operator!=(const PhysicalNumber& rhs);
+
         bool operator<(const PhysicalNumber& rhs);
+
         bool operator>(const PhysicalNumber& rhs);
+
         bool operator<=(const PhysicalNumber& rhs);
+
         bool operator>=(const PhysicalNumber& rhs);
+
+        int  samefamily  (const PhysicalNumber& rhs);
+
         //+=,-=,+,-
+
         PhysicalNumber &operator+=(const PhysicalNumber& rhs);
-        PhysicalNumber &operator-=(PhysicalNumber& rhs);
-        PhysicalNumber &operator+(const PhysicalNumber& rhs);
-        PhysicalNumber &operator-(const PhysicalNumber& rhs);
-        //onary 
-        PhysicalNumber& operator-();
+
+        PhysicalNumber &operator-=(const PhysicalNumber& rhs);
+
+        PhysicalNumber& operator+(const PhysicalNumber& rhs);
+
+        PhysicalNumber& operator-(const PhysicalNumber& rhs);
+
+        //onary
+
+        PhysicalNumber operator-();
+
         PhysicalNumber& operator+();
+
         //++,--
+
+        PhysicalNumber& operator++(int);
+
         PhysicalNumber& operator++();
+
+        PhysicalNumber& operator--(int);
+
         PhysicalNumber& operator--();
+
+        void convert(double& ,double& ,int ,const PhysicalNumber& );
+
         //stream
-        friend std::istream& operator>>(std::istream& in, const PhysicalNumber& a);
+    PhysicalNumber& help(double ,int);
+        friend istream& operator>>(istream& in,  PhysicalNumber& a);
+
         friend ostream& operator<<(ostream& out, const PhysicalNumber& a);
-       
+
+
     };
+
+
+
 }
